91ca73f79113d83e99d4d71ac6d8aee94576d557
Clement Dumas <butanium.contact@gmail.com>
2025-01-09 10:27:34 -0800

    fixed heuristic

--- test_heuristic.py
+++ test_heuristic.py
@@ -18,7 +18,7 @@ def heuristic_pallier():
         env = FastHIVPatient()
         env.state_vec = state_vec
         for step in range(current_time, 200):
-            _obs, reward, _, _, _ = env.step(2)
+            _obs, reward, _, _, _ = env.step(0)
             current_reward += reward
         return current_time, current_reward
 
@@ -46,8 +46,8 @@ def heuristic_pallier():
     plt.savefig("results_heuristic.png")
 
 
-def greedy_heuristic(consecutive_actions=1, num_watch_steps=10):
-    env = FastHIVPatient()
+def greedy_heuristic(consecutive_actions=1, num_watch_steps=5, domain_randomization=False):
+    env = FastHIVPatient(domain_randomization=domain_randomization)
     env.reset()
     cum_reward = 0
     actions = []
@@ -109,7 +109,6 @@ def main_greedy():
     best_sol_cum_reward = 0
     best_sol_consecutive_actions = 0
     best_sol_actions = []
-    res_dicts = []
     for nws, (best_consecutive_actions, best_cum_reward, best_actions, res_dict) in zip(
         num_watch_steps_list, results
     ):
@@ -118,7 +117,6 @@ def main_greedy():
             best_sol_consecutive_actions = best_consecutive_actions
             best_sol_actions = best_actions
             best_sol_nws = nws
-            res_dicts.append(res_dict)
     print("\n\n**************")
     print(
         f"Best solution: {best_sol_nws} {best_sol_consecutive_actions} {best_sol_cum_reward:.2e} {best_sol_actions}"
@@ -126,12 +124,12 @@ def main_greedy():
     print("**************")
     try:
         with open("results_greedy.json", "w") as f:
-            json.dump(res_dicts, f)
+            json.dump(results, f)
     except Exception as e:
         print(f"Error saving results: {e}")
         try:
             with open("results_greedy.pkl", "wb") as f:
-                pickle.dump(res_dicts, f)
+                pickle.dump(results, f)
         except Exception as e:
             print(f"Error saving results with pickle: {e}")
     # plot as a heatmap
@@ -141,7 +139,7 @@ def main_greedy():
                 consecutive_actions: cum_reward
                 for consecutive_actions, (cum_reward, _) in res_dict.items()
             }
-            for nws, res_dict in zip(num_watch_steps_list, res_dicts)
+            for nws, res_dict in zip(num_watch_steps_list, results)
         }
     )
     plt.savefig("plots/results_greedy_heatmap.png")
@@ -150,10 +148,24 @@ def main_greedy():
         best_sol_consecutive_actions,
         best_sol_cum_reward,
         best_sol_actions,
-        res_dicts,
+        results,
     )
 
 
+def main_greedy_domain_randomization(consecutive_actions=1, num_watch_steps=5):
+    results = Parallel(n_jobs=-1)(
+        delayed(greedy_heuristic)(consecutive_actions, num_watch_steps, True)
+        for _ in range(100)
+    )
+    print(f'mean reward: {np.mean([res[1] for res in results])}')
+    with open("results_greedy_domain_randomization.json", "w") as f:
+        json.dump(results, f)
+    
+    # plot histogram of results
+    plt.hist([res[1] for res in results])
+    plt.savefig("plots/results_greedy_domain_randomization.png")
+
 if __name__ == "__main__":
-    heuristic_pallier()
-    main_greedy()
+    # heuristic_pallier()
+    # main_greedy()
+    main_greedy_domain_randomization()
